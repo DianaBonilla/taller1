e917d6637f08d28a99de9d4561881eac2365454c
USUARIO06-PC\Usuario01 <david-hernandezbgu@hotmail.com>
2016-12-14 12:08:03 -0500

    ejercicio 3 y 4

--- Ejercicios.py
+++ Ejercicios.py
@@ -21,3 +21,39 @@ msg=Message(master, text=whatever_you_do)
 msg.config(bg='lightgreen', font=('times',24, 'italic'))
 msg.pack()
 mainloop()
+
+print("\nEjercicio 3")
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.button = Button(frame, text="SALIR",
+    fg="red",command=frame.quit)
+        self.button.pack(side=LEFT)
+        self.slogan =Button(frame,text="ENTRAR",command=self.write_slogan)
+        self.slogan.pack(side=LEFT)
+    def write_slogan(self):
+            print ("Estamos aprendiendo a usar Tkinter!")
+            
+print("\nEjercicio 4")
+root = Tk()
+app = App(root)
+root.mainloop()
+
+root = Tk()
+v = IntVar()
+Label(root,
+      text="""Choose a programming language:""",
+      justify = LEFT,
+      padx = 20).pack()
+Radiobutton(root,
+            text="Python",
+            padx = 20, variable=v,
+            value=1).pack(anchor=W)
+Radiobutton(root,
+            text="Perl",
+            padx = 20,
+            variable=v,
+            value=2).pack(anchor=W)
+mainloop()
+
